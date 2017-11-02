c953b95f2070e6a33e4d0e852e14cf5f227b2532
Mahesh Kondamuri <Mahesh@Maheshs-MacBook-Pro.local>
2017-11-02 13:15:54 +0530

    First Commit mahesh

--- Tuto Learn/ViewController.swift
+++ Tuto Learn/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //Mahesh
         self.setLayoutAndSetTexts()
         self.setUpForGoogleSignIn()
     }
