301d51b0260a3bdb689840e37ec25775ae08dbf7
mahesh789 <maheshiphonedev@gmail.com>
2017-11-09 00:50:26 +0530

    123456-reg

--- Tuto Learn/TutorViewControllers/StudentRegistrationViewController.swift
+++ Tuto Learn/TutorViewControllers/StudentRegistrationViewController.swift
@@ -104,27 +104,23 @@ class StudentRegistrationViewController: UIViewController, UITextFieldDelegate {
     
     func registrationApicall() -> Void {
         let urlPath = String(format: "%@%@",Constants.baseUrl,Constants.studentRegister) as String
-        let data = UIImagePNGRepresentation(UIImage(named: "menu")!) as NSData?
-        let imageStr = data?.base64EncodedString(options: NSData.Base64EncodingOptions(rawValue: 0))
-
+        
         let dictionary = NSMutableDictionary()
+        dictionary.setValue("student", forKey: "register_type")
         dictionary.setValue(self.firstNameTextField.text, forKey: "s_name")
+        dictionary.setValue(self.lastNameTextField.text, forKey: "s_lastname")
         dictionary.setValue(self.mobNoTextField.text, forKey: "s_mobile")
         dictionary.setValue(self.eMailTextField.text, forKey: "s_email")
         dictionary.setValue(self.passwordTextField.text, forKey: "s_password")
-        dictionary.setValue(self.address1TextField.text, forKey: "s_address1")
-        dictionary.setValue(self.address2TextField.text, forKey: "s_address2")
-        dictionary.setValue("12", forKey: "s_city_ide")
-        dictionary.setValue("a4b12c9d", forKey: "s_login_key")
-        dictionary.setValue("400001", forKey: "s_pin")
-        dictionary.setValue(self.lastNameTextField.text, forKey: "s_lastname")
         dictionary.setValue(self.dobTextField.text, forKey: "s_dob")
         dictionary.setValue(self.genderTextField.text, forKey: "s_gender")
-        dictionary.setValue("New English School", forKey: "s_school_name")
-        dictionary.setValue("9", forKey: "s_level")
-        dictionary.setValue(imageStr, forKey: "s_profile_img")
-        dictionary.setValue("png", forKey: "s_extension")
-        
+        dictionary.setValue(self.address1TextField.text, forKey: "s_address1")
+        dictionary.setValue(self.address2TextField.text, forKey: "s_address2")
+        dictionary.setValue("400001", forKey: "s_pin")
+        dictionary.setValue("L6783452H", forKey: "s_nric")
+        dictionary.setValue("Mobile", forKey: "s_oauth")
+        dictionary.setValue("12", forKey: "s_city_ide")
+
         Alamofire.request(urlPath, method: .post, parameters: (dictionary as! [String:Any]), encoding: JSONEncoding.default, headers:["Content-Type":"application/json"]) .responseJSON { response in
                 if response.result.isSuccess
                 {
@@ -132,14 +128,14 @@ class StudentRegistrationViewController: UIViewController, UITextFieldDelegate {
                     {
                         self.showAlertController(alertMessage: resultDictionary["message"] as? String)
                     }
-                }else if response.result.isFailure
-                {
+                }
+                else if response.result.isFailure {
                     print(response.result.error as Any)
                 }
                 MBProgressHUD.hide(for: self.view, animated: true)
         }
     }
-    
+
     //MARK: Default AlertViewController
     func showAlertController(alertMessage:String?) -> Void {
         let alert = UIAlertController(title: "", message: alertMessage, preferredStyle: UIAlertControllerStyle.alert)
